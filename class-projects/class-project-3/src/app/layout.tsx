8b3c7ccc95543f2a4ef9b5ad732fe1679e954173
ToniGrbic <tonigrbic.5@gmail.com>
2023-10-23 11:19:31 +0200

    sitemap impemented with navbar

--- class-projects/class-project-3/src/app/layout.tsx
+++ class-projects/class-project-3/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} "flex min-h-screen items-center justify-between p-24`}
+      >
+        <nav>
+          <ul className="flex gap-10 text-lg">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/services">Services</Link>
+            </li>
+            <li>
+              <Link href="/products">Products</Link>
+            </li>
+            <li>
+              <Link href="/designs">Designs</Link>
+            </li>
+            <li>
+              <Link href="/portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link href="/tools">Tools</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <main className="flex justify-center mt-12">{children}</main>
+      </body>
     </html>
   );
 }
