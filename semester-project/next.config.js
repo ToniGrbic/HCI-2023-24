ba8a947397a97a9a2a0516485ee2f0e0254e7296
ToniGrbic <tonigrbic.5@gmail.com>
2026-04-06 16:08:31 +0200

    lint fix

--- semester-project/next.config.js
+++ semester-project/next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	eslint: {
-		dirs: ["src/app", "src/components", "src/hooks", "src/lib", "src/styles", "src/types"],
-	},
-}
+  eslint: {
+    dirs: [
+      "src/app",
+      "src/components",
+      "src/hooks",
+      "src/lib",
+      "src/styles",
+      "src/types",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
