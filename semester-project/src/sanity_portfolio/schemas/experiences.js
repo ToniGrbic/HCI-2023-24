89a43d307a22b689f0a7a08d423742051e35ba34
ToniGrbic <tonigrbic.5@gmail.com>
2026-04-12 22:49:19 +0200

    remove required image from experiences schema

--- semester-project/src/sanity_portfolio/schemas/experiences.js
+++ semester-project/src/sanity_portfolio/schemas/experiences.js
@@ -36,7 +36,7 @@ export default {
       title: "ImageUrl",
       type: "image",
       codegen: { required: true },
-      validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.optional(),
       options: {
         hotspot: true,
       },
